bf9c3df553925b3222ecc3b8dd1ec282c91d7cf9
Yuri Maciel <yurimpro@outlook.com>
2023-11-01 23:34:26 -0300

    update home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,36 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">I will work in</label>
+          <input id="task" />
+          <label htmlFor="">for</label>
+          <input type="number" id="minutesAmount" />
+          <span>minutes</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Start
+        </button>
+      </form>
+    </HomeContainer>
+  )
 }
