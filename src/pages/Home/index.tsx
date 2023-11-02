b10eabc39539000ee98ede73f9affeb4805a066b
Yuri Maciel <yurimpro@outlook.com>
2023-11-02 00:08:55 -0300

    add datalist to input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,12 +15,27 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">I will work in</label>
-          <TaskInput placeholder="Give a name to your project" id="task" />
+          <TaskInput
+            list="task-suggestions"
+            placeholder="Give a name to your project"
+            id="task"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
+
           <label htmlFor="">for</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutes</span>
         </FormContainer>
