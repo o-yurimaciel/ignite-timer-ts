f77be4a593a36080146133a5617607f94da4131b
Yuri Maciel <yurimpro@outlook.com>
2023-11-02 01:41:30 -0300

    add history table

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,71 @@
+import { HistoryContainer, HistoryList, Status } from './style'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>My History</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Start</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="red">Not finished</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="yellow">Pending</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="green">Finished</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="red">Not finished</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="yellow">Pending</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="green">Finished</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
