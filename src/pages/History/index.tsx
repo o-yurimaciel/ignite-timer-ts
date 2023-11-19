4e859798a5239d5f0141d86c8a7be9b3e4c293dc
Yuri Maciel <yurimpro@outlook.com>
2023-11-19 15:53:01 -0300

    format startDate with formatDistanceToNow date-fns

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { HistoryContainer, HistoryList, Status } from './style'
 import { CyclesContext } from '../../contexts/CycleContext'
+import { formatDistanceToNow } from 'date-fns'
 
 export function History() {
   const { cycles } = useContext(CyclesContext)
@@ -24,7 +25,11 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutes</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                    })}
+                  </td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Finished</Status>
